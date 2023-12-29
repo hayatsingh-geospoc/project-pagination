e13f33b45711e0790d6c35964f869cbcbb449040
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2023-12-29 23:35:02 +0530

    server created

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require('express');
-const 
+const app = express();
+
+
+app.get('/data',(req,res)=> {
+  return res.send("hello js")
+})
+
+app.listen(3000,()=> {
+    console.log(`Server  is running on Port 3000`)
+})
