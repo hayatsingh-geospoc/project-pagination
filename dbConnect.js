bf71c834b7603b1fa052a1bd522cd166d2c4de3a
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-01-03 23:50:46 +0530

    Db connection created

--- dbConnect.js
+++ dbConnect.js
@@ -1,4 +1,14 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
 
-mongoose.connect(process.env.PORT,)
+const dbConnect = () => {
+  mongoose.connect(process.env.PORT, { useNewUrlParser: true });
+
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to DB successfully');
+  });
+
+  mongoose.connection.on('Error', (err) => {
+    console.log('Error while connecting to Database', err);
+  });
+};
