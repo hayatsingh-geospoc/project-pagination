0b6faf3a1ced51414d0370398720de051078d245
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-01-04 13:24:06 +0530

    db Module comment

--- dbConnect.js
+++ dbConnect.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+// const mongoose = require('mongoose');
+// require('dotenv').config();
 
 
 
-const dbConnect = () => {
-  mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+// const dbConnect = () => {
+//   mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 
-  mongoose.connection.on('connected', () => {
-    console.log('Connected to DB successfully');
-  });
+//   mongoose.connection.on('connected', () => {
+//     console.log('Connected to DB successfully');
+//   });
 
-  mongoose.connection.on('Error', (err) => {
-    console.log('Error while connecting to Database', err);
-  });
+//   mongoose.connection.on('Error', (err) => {
+//     console.log('Error while connecting to Database', err);
+//   });
 
-  mongoose.connection.on('Disconnected', () => {
-    console.log('MongoDb connection disconnected');
-  });
-};
+//   mongoose.connection.on('Disconnected', () => {
+//     console.log('MongoDb connection disconnected');
+//   });
+// };
 
-module.exports = dbConnect;
+// module.exports = dbConnect;
